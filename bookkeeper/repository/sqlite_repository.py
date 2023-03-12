1bbac299c463a82e18c664d49f4ce7b9bb0e5ab9
setupishe <91007993+setupishe@users.noreply.github.com>
2023-03-12 18:24:44 +0300

    added table creation, add, get and get_all methods

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -6,18 +6,79 @@ class SQLiteRepository(AbstractRepository[T]):
     def __init__(self, db_file: str, cls: type) -> None:
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
+        self.cls = cls
         self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop('pk')
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            res = cur.execute('SELECT name FROM sqlite_master')
+            db_tables = [t[0].lower() for t in res.fetchall()]
+            if self.table_name not in db_tables:
+                col_names = ', '.join(self.fields.keys())
+                q = f'CREATE TABLE {self.table_name} (' \
+                    f'"pk" INTEGER PRIMARY KEY AUTOINCREMENT, {col_names})'
+                cur.execute(q)
+        con.close()
+
     def add(self, obj: T) -> int:
         names = ', '.join(self.fields.keys())
         p = ', '.join("?" * len(self.fields))
-
         values = [getattr(obj, x) for x in self.fields]
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
-        cur.execute('PRAGMA foreign_keys = ON')
-        cur.execute(
-            f'INSERT INTO {self.table_name} ({names}) VALUES({p})', values)
-        obj.pk = cur.lastrowid
+            cur.execute('PRAGMA foreign_keys = ON')
+            q = f'INSERT INTO {self.table_name} ({names}) VALUES ({p})'
+            cur.execute(q, values)
+            obj.pk = cur.lastrowid
         con.close()
         return obj.pk
+
+    def __generate_object(self, db_row: tuple) -> T:
+        obj = self.cls(self.fields)
+        for field, value in zip(self.fields, db_row[1:]):
+            setattr(obj, field, value)
+        obj.pk = db_row[0]
+        return obj
+
+    def get(self, pk: int) -> T | None:
+        """ Получить объект по id """
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            q = f'SELECT * FROM {self.table_name} WHERE pk = {pk}'
+            row = cur.execute(q).fetchone()
+        con.close()
+
+        if row is None:
+            return None
+
+        return self.__generate_object(row)
+
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        """
+        Получить все записи по некоторому условию
+        where - условие в виде словаря {'название_поля': значение}
+        если условие не задано (по умолчанию), вернуть все записи
+        """
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            q = f'SELECT * FROM {self.table_name}'
+            if where is not None:
+                q_add = f'WHERE '
+                for i, key in where:
+                    q_add += key + '==' + where[key]
+                    if i < len(where) - 1:
+                        q_add += ' AND '
+                q += q_add
+
+            rows = cur.execute(q).fetchall()
+
+            res = []
+            if rows is None:
+                return res
+
+            for row in rows:
+                res.append(self.__generate_object(row))
+
+            return res
+
+
